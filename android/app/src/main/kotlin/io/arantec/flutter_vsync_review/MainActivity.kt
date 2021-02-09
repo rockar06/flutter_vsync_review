f2ff66fc3ea9b5ed96698451e8e347bceb823be1
Armando Ruiz <aranruiz13@gmail.com>
2021-02-09 11:54:14 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/io/arantec/flutter_vsync_review/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.arantec.flutter_vsync_review
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vsync Sample'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RaisedButton(
+              onPressed: () {},
+              child: const Text('Manual Animated Clock'),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: const Text('Vsync Animated Clock'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
